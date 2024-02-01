596fa2fb009b4f8c224a2c06a1013daad4c1a7ff
zxcmaxim <maksikplayb@gmail.com>
2024-02-01 18:52:20 +0300

    task2

---
+++ 2-switch/switch.js
@@ -0,0 +1,19 @@
+let language = 'rus'
+
+switch (language) {
+    case 'rus':
+        console.log('а можно мне швеепс');
+        break;
+    case 'eng':
+        console.log('give me some schweppes');
+        break;
+    case 'germ':
+        console.log('kann ich einen Schweps haben');
+        break;
+    case 'arab':
+        console.log('هل يمكنني الحصول على شويبس');
+        break;
+    default:
+        console.log('неизвестный язык');
+        break;
+}
