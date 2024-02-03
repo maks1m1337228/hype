92a29ed9c793b89239c3396540d4114c67e2ddd2
zxcmaxim <maksikplayb@gmail.com>
2024-02-03 13:08:25 +0300

    убрал let

--- 4-navigator/nav.js
+++ 4-navigator/nav.js
@@ -2,5 +2,8 @@ const addressLat = 10
 const addressLong = 20
 const positionLat = 30
 const positionLong = 15
-let distance = Math.sqrt(Math.pow((positionLat - addressLat), 2) + Math.pow((addressLong - positionLong), 2))
+const distance = Math.sqrt(
+	Math.pow(positionLat - addressLat, 2) +
+		Math.pow(addressLong - positionLong, 2)
+)
 console.log(distance)
