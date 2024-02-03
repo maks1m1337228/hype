5b8932320a9bbf09255f39b609a41210485f95d4
zxcmaxim <maksikplayb@gmail.com>
2024-02-03 15:14:51 +0300

    стрелочная функция

--- deposit/deposit.js
+++
@@ -1,6 +0,0 @@
-let deposit = 12000
-const percent = 0.07
-const target = 13500
-const finalSum = deposit * (1 + percent / 12) ** 24
-console.log(finalSum > target ? 'Денег хватит' : 'Денег не хватит')
-console.log('Остаток после покупки: ' + (finalSum - target) + '$')
--- robot/robot.js
+++
@@ -1,6 +0,0 @@
-const answer = prompt('Сколько будет 7 + 15', '')
-if (answer == '22' || answer == 'Я не робот') {
-	console.log('Успех')
-} else {
-	console.log('Вы робот!')
-}
