1cf7ed9512973788d8b678a5cb398b583aa99265
zxcmaxim <maksikplayb@gmail.com>
2024-02-05 21:52:53 +0300

    commit

--- robot/robot.js
+++
@@ -1,7 +0,0 @@
-const answer = prompt('Сколько будет 7 + 15', '')
-answer = Number(answer)
-if (answer === '22' || answer ==='Я не робот') {
-	console.log('Успех')
-} else {
-	console.log('Вы робот!')
-}
