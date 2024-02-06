dda26104c51bd19ffc5daaeeeeccccf21980cc02
zxcmaksim <zxc@gmail.com>
2024-02-06 15:37:13 +0300

    убрал скобки

--- robot/robot.js
+++ robot/robot.js
@@ -1,4 +1,4 @@
-const answer = prompt('Сколько будет 7 + 15', '')
+const answer = prompt('Сколько будет 7 + 15')
 if (Number(answer) === 22 || answer ==='Я не робот') {
   console.log('Успех')
 } else {
