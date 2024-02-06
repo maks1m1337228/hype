7fecb672a824e86684d0dd940a3df431921919bc
zxcmaksim <zxc@gmail.com>
2024-02-06 11:25:51 +0300

    ispravil

--- robot/robot.js
+++ robot/robot.js
@@ -1,7 +1,6 @@
 const answer = prompt('Сколько будет 7 + 15', '')
-answer = Number(answer)
-if (answer === '22' || answer ==='Я не робот') {
-	console.log('Успех')
+if (Number(answer) === 22 || answer ==='Я не робот') {
+  console.log('Успех')
 } else {
-	console.log('Вы робот!')
+  console.log('Вы робот!')
 }
