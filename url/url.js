2943d2939ac11760b43d5e35fe8d74ed6aea98c9
zxcmaxim <maksikplayb@gmail.com>
2024-02-06 17:41:55 +0300

    url

---
+++ url/url.js
@@ -0,0 +1,13 @@
+const url = "https://studentki.ru/teens"
+
+function toArray(url) {
+	const arr = url.split('/');
+	for (let i = 0; i < arr.length; i++) {
+		console.log(arr[i])
+	}
+}
+toArray(url)
+
+
+		
+	
