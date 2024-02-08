4fdf80f19c6ddf59b7d277bdb189536d58e2c3aa
zxcmaxim <maksikplayb@gmail.com>
2024-02-08 14:41:26 +0300

    commit

---
+++ crypto/crypto.js
@@ -0,0 +1,20 @@
+const password = prompt('give me your password')
+password = "password"
+function crypto(password) {
+	const [letOne, letTwo, letThree, letFour, letFive, letSix, letSeven, letEight] = password
+	for (let i = 0; i < password.length; i++) {
+		const index = password.indexOf(password[])
+    if (index !== -1) {
+        const transfer = tasks.splice(index, 1) 
+        tasks.unshift(transfer[0])
+		
+	}
+     
+
+    }
+
+
+
+	console.log(password);
+}
+crypto(password)
