4ec23d47ebf384f087e2443e5c00b4fc0f066a7d
zxcmaxim <maksikplayb@gmail.com>
2024-02-09 20:56:17 +0300

    url

--- url/url.js
+++ url/url.js
@@ -1,12 +1,14 @@
 const url = "https://studentki.ru/teens/hui";
 
-function devidedStudentki (url) {
-	const [protocol, _, host, ...path] = url.split('/')
-	console.log(protocol.replace(":", ""));
-	console.log(host)
-	console.log('/' + path.join('/'));
+function devidedStudentki(url) {
+  const [protocol, _, host, ...path] = url.split('/');
+  console.log(`${protocol.replace(":", "")}`);
+  console.log(`${host}`);
+  console.log(`/${path.join('/')}`);
 }
-devidedStudentki(url)
+
+devidedStudentki(url);
+
 
 
 
