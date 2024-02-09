e3373aaac3e1f081f993ead300ea52a6fc7b32f6
zxcmaxim <maksikplayb@gmail.com>
2024-02-09 20:15:00 +0300

    pass

--- crypto/crypto.js
+++ crypto/crypto.js
@@ -1,29 +1,26 @@
-const password = 'password'
-let codedPassword
+const password = 'qwe123'
 
-function crypto() {
-	const arr = password.split('')
-	const arr1 = arr.slice(0, 4)
-	const arr2 = arr.slice(4)
-	arr1.reverse()
-	arr2.reverse()
-	const arr3 = arr1.concat(arr2)
-	codedPassword = arr3.join('')
-	console.log(codedPassword)
+function getTwoPartsOfPassword(password) {
+  const middle = Math.floor(password.length / 2)
+  const leftPart = password.slice(0, middle)
+  const rightPart = password.slice(middle)
+
+  return [leftPart, rightPart]
 }
 
-function check() {
-	const arr = codedPassword.split('')
-	const arr1 = arr.slice(0, 4)
-	const arr2 = arr.slice(4)
-	arr1.reverse()
-	arr2.reverse()
-	const arr3 = arr1.concat(arr2)
-	codedPassword = arr3.join('')
-	return codedPassword === password
+function cryptPassword(password) {
+  let [leftPart, rightPart] = getTwoPartsOfPassword(password);
+	[leftPart, rightPart] = [rightPart, leftPart]
+  leftPart = leftPart.split('').reverse().join('');
+  rightPart = rightPart.split('').reverse().join('');
+  return leftPart + rightPart;
 }
-crypto()
-const compare = check()
-console.log(compare);
 
+const comparePassword = (cryptedPassword, password) =>
+  cryptedPassword === cryptPassword(password)
 
+const cryptedPassword = cryptPassword(password)
+
+const res = comparePassword('321ewq', password)
+
+console.log(res)
