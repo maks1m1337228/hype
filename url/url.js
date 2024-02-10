9768b7f41eff411f7354322809ac37099a81af8f
zxcmaxim <maksikplayb@gmail.com>
2024-02-10 12:55:51 +0300

    peredelal skibidi

--- url/url.js
+++ url/url.js
@@ -2,8 +2,8 @@ const url = "https://studentki.ru/teens/hui";
 
 function devidedStudentki(url) {
   const [protocol, _, host, ...path] = url.split('/');
-  console.log(`${protocol.replace(":", "")}`);
-  console.log(`${host}`);
+  console.log(protocol.replace(":", ""));
+  console.log(host);
   console.log(`/${path.join('/')}`);
 }
 
